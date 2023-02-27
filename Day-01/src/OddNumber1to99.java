e5d89aef73d404ca250af37b27258d370facd9e5
Mariyam_Sardar <87272687+Mariyam97@users.noreply.github.com>
2023-02-27 18:18:59 +0530

    Add files via upload

---
+++ Day-01/src/OddNumber1to99.java
@@ -0,0 +1,12 @@
+
+public class OddNumber1to99 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		for(int i=1;i<100;i+=2) {
+			System.out.println("The odd no from 1 to 99 are: "+ i);
+		}
+	}
+
+}
